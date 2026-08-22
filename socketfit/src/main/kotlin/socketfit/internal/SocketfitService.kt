5ef8e64bd241b51e9b41ef1ccb0dc766c28b492d
Bob Riverfinch <bob.riverfinch@gmail.com>
2026-08-22 18:17:42 +0300

    Add Socketfit service implementation

---
+++ socketfit/src/main/kotlin/socketfit/internal/SocketfitService.kt
@@ -0,0 +1,22 @@
+package socketfit.internal
+
+import socketfit.Service
+import socketfit.ServiceMethod
+import java.lang.reflect.Method
+
+internal class SocketfitService(
+    private val serviceMethods: Map<Method, ServiceMethod>
+) : Service {
+    override fun invoke(method: Method, args: Array<Any>): Any {
+        val serviceMethod = serviceMethods[method]
+        require(serviceMethod != null) {
+            buildString {
+                append("Could not find service method for ")
+                append(method.declaringClass.name)
+                append(".")
+                append(method.name)
+            }
+        }
+        return serviceMethod.invoke(args)
+    }
+}
