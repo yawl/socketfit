f56d1a3e264e035125e113edff0670770e7198ee
Bob Riverfinch <bob.riverfinch@gmail.com>
2026-08-28 21:40:42 +0300

    Add Equal and Full jitter implementations

---
+++ socketfit/src/main/kotlin/socketfit/backoff/EqualJitter.kt
@@ -0,0 +1,24 @@
+package socketfit.backoff
+
+import socketfit.Jitter
+import kotlin.random.Random
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+
+/**
+ * Applies Equal Jitter to a backoff duration.
+ *
+ * @param random the source of randomness used to select the delay
+ *
+ * @see <a href="https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/">
+ * AWS, Exponential Backoff and Jitter
+ * </a>
+ */
+class EqualJitter(
+    private val random: Random,
+) : Jitter {
+    override fun apply(backoff: Duration): Duration {
+        val halfMilliseconds = backoff.inWholeMilliseconds / 2
+        return (halfMilliseconds + random.nextLong(0, halfMilliseconds)).milliseconds
+    }
+}
---
+++ socketfit/src/main/kotlin/socketfit/backoff/FullJitter.kt
@@ -0,0 +1,24 @@
+package socketfit.backoff
+
+import socketfit.Jitter
+import kotlin.random.Random
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+
+/**
+ * Applies Full Jitter to a backoff duration.
+ *
+ * @param random the source of randomness used to select the delay
+ *
+ * @see <a href="https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/">
+ * AWS, Exponential Backoff and Jitter
+ * </a>
+ */
+class FullJitter(
+    private val random: Random,
+) : Jitter {
+    override fun apply(backoff: Duration): Duration {
+        val milliseconds = backoff.inWholeMilliseconds
+        return random.nextLong(0, milliseconds).milliseconds
+    }
+}
