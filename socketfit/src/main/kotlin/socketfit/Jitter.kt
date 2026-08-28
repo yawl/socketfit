410f3afb913630a80bb2f31decdf9b1770478d7d
Bob Riverfinch <bob.riverfinch@gmail.com>
2026-08-28 21:40:30 +0300

    Add Jitter contract

---
+++ socketfit/src/main/kotlin/socketfit/Jitter.kt
@@ -0,0 +1,20 @@
+package socketfit
+
+import kotlin.time.Duration
+
+/**
+ * Applies jitter to a backoff duration.
+ *
+ * @see <a href="https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/">
+ * AWS, Exponential Backoff and Jitter
+ * </a>
+ */
+interface Jitter {
+    /**
+     * Applies this jitter strategy to the specified backoff duration.
+     *
+     * @param backoff the backoff duration
+     * @return the jittered backoff duration
+     */
+    fun apply(backoff: Duration): Duration
+}
